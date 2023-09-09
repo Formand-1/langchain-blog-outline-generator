d0d6bb94f43dc846c59779517d92a80a0108891e
Steffan Dissing <132760085+Formand-1@users.noreply.github.com>
2023-09-09 17:19:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,14 +40,17 @@ def generate_response(topic):
             # Check the expected structure of the response
             if isinstance(response, dict) and 'choices' in response and isinstance(response['choices'], list) and len(response['choices']) > 0:
                 content = response['choices'][0]['message']['content'].strip()
-                st.info(content)
+                # Render the content as markdown
+                st.markdown(content)
                 # Adding a button to copy content to clipboard
-                if st.button("Copy to Clipboard"):
-                    st.clightboard.write_text(content)
+                st.write('<a href="javascript:void(0)" onclick="navigator.clipboard.writeText(`' + content + '`)">Copy to clipboard</a>', unsafe_allow_html=True)
             else:
                 st.error("Unexpected response format from the model.")
+                # Diagnostic logging
+                st.write("Raw response from OpenAI:", response)
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
+
 
 with st.form('myform'):
     topic_text = st.text_input('Enter keyword or topic:', '')
