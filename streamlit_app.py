5731df424790fd0757de318d80d949685d1414d0
Steffan Dissing <132760085+Formand-1@users.noreply.github.com>
2023-09-09 13:29:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 def generate_response(topic):
   llm = OpenAI(model_name='text-davinci-003', openai_api_key=openai_api_key)
   # Prompt
-  template = 'Simulate an exceptionally talented technical writer and editor, generate an outline for a blog about {topic}. Given the following instructions, think step by step and produce the best possible output you can. Return the results in nicely formatted markdown please'
+  template = 'Simulate an exceptionally talented technical writer and editor, generate an outline for a blog about {topic}. Given the following instructions, think step by step and produce the best possible output you can. Return the results in nicely formatted markdown please. Start with a short introduction without subpoints, then main point 1, main point 2, main point 3 and so on. Add subpoints to each main point when it make sense. Then add a FAQ with 3-5 'people also ask qustions about {topic} and lastly a conclussion and footnotes.' '
   prompt = PromptTemplate(input_variables=['topic'], template=template)
   prompt_query = prompt.format(topic=topic)
   # Run LLM model and print out response
